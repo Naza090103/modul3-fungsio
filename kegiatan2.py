9be708c54d67ee0e76f0b4370b554dbb9c32f921
Naza090103 <149454180+Naza090103@users.noreply.github.com>
2023-10-31 10:38:54 +0700

    Add files via upload

---
+++ kegiatan2.py
@@ -0,0 +1,29 @@
+data = ["3 minggu 3 hari 7 jam 21 menit",
+        "5 minggu 5 hari 8 jam 11 menit",
+        "7 minggu 1 hari 5 jam 33 menit"]
+
+def curried_converter(weeks):
+    def inner_curried(days):
+        def inner_inner_curried(hours):
+            def final_curried(minutes):
+                return [weeks, days, hours, minutes]
+            return final_curried
+        return inner_inner_curried
+    return inner_curried
+
+def filter_integer_values(item):
+    return list(filter(lambda x: x.isdigit(), item.split()))
+
+int_values = []
+
+for item in data:
+    integer_values = list(map(int, filter_integer_values(item)))
+    weeks = integer_values[0]
+    days = integer_values[1]
+    hours = integer_values[2]
+    minutes = integer_values[3]
+
+    converted_value = curried_converter(weeks)(days)(hours)(minutes)
+    int_values.append(converted_value)
+
+print(int_values)
---
+++ kegiatan3.py
@@ -0,0 +1,20 @@
+random_list = [105, 3.1, "Hello", 737, "Python", 2.7, "World", 412, 5.5, "AI"]
+
+def separate_int(item):
+    if isinstance(item, int):
+        satuan = item % 10
+        puluhan = (item // 10) % 10
+        ratusan = item // 100
+        return (item, {"ratusan": ratusan, "puluhan": puluhan, "satuan": satuan})
+    else:
+        return item
+
+int_list = list(filter(lambda x: isinstance(x, tuple), map(separate_int, random_list)))
+
+float_tuple = tuple(filter(lambda item: isinstance(item, float), random_list))
+
+str_list = list(filter(lambda item: isinstance(item, str), random_list))
+
+print("Data int dalam bentuk list of tuples:", int_list, end='\n\n')
+print("Data float dalam bentuk tuple:", float_tuple, end='\n\n')
+print("Data string dalam bentuk list:", str_list)
